6bf7583c3562d7ccff453e05a4bc66523a6775fa
Paulo Renato <paulorsouzaf@gmail.com>
2020-03-07 02:24:19 -0300

    Wrote test for new user registration

---
+++ test/integration/UserController.test.js
@@ -0,0 +1,32 @@
+const app = require('../../src/app');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const factory = require('../utils/factories');
+
+describe('user registration', function() {
+    it('should register a user with valid credentials', async function() {
+        const validUser = await factory.build('User', {
+            username: 'john_doe'
+        });
+        
+        return request(app)
+            .post('/register')
+            .send(validUser)
+            .expect(200);
+    });
+
+    it('should not register a user with an existing username', async function() {
+        const existingUser = await factory.build('User', {
+            username: 'john_doe'
+        });
+        
+        return request(app)
+            .post('/register')
+            .send(existingUser)
+            .expect(400);
+    })
+
+    after(function(){
+        mongoose.connection.db.dropDatabase();
+    });
+})
