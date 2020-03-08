0eb0eca6493f023eba2ed2dea4952fc05d8db8d3
Paulo Renato <paulorsouzaf@gmail.com>
2020-03-08 19:54:33 -0300

    Wrote tests for user login

---
+++ test/integration/SessionController.test.js
@@ -0,0 +1,47 @@
+const app = require('../../src/app');
+const request = require('supertest');
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const factory = require('../utils/factories');
+
+describe('user login', function() {
+    it('should get a jwt token for a registered user', async function() {
+        const user = {
+            username: 'johndoe',
+            password: 'johnssecret'
+        }
+
+        await factory.create('User', user);
+
+        const res = await request(app)
+            .post('/login')
+            .send(user)
+            .expect(200);
+
+        assert.property(res.body, 'accessToken');
+    });
+
+    it('should throw an error when logging in inexistent user', async function() {
+        const user = await factory.build('User');
+        
+        return request(app)
+            .post('/login')
+            .send(user)
+            .expect(404);
+    });
+
+    it('should throw an error when logging in user with incorrect password', async function() {
+        const user = await factory.create('User');
+
+        user.password = 'wrongpassword';
+
+        return request(app)
+            .post('/login')
+            .send(user)
+            .expect(401);
+    })
+
+    after(function(){
+        mongoose.connection.db.dropDatabase();
+    });
+});
