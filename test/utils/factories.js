6a2e850a5ae2385a63c537212a38dd7c2cc1f849
Paulo Renato <paulorsouzaf@gmail.com>
2020-03-09 21:29:55 -0300

    Changed user factory to generate new data at every build call

--- test/utils/factories.js
+++ test/utils/factories.js
@@ -2,10 +2,13 @@ const { factory } = require('factory-girl');
 const User = require('../../src/models/User');
 const faker = require('faker');
 
-factory.define('User', User, {
-    name: faker.name.findName(),
-    username: faker.internet.userName(),
-    password: faker.internet.password()
+factory.define('User', User, function() {
+    return {
+        name: faker.name.findName(),
+        username: faker.internet.userName(),
+        password: faker.internet.password()
+    };
+});
 });
 
 module.exports = factory;
