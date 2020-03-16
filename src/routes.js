dc573af3bc9849949293e9aad4eeae36e5d0bdf3
Paulo Renato <paulorsouzaf@gmail.com>
2020-03-16 19:18:43 -0300

    Add routes for the note controller

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 const { Router } = require('express');
 const UserController = require('./controllers/UserController');
 const SessionController = require('./controllers/SessionController');
+const NoteController = require('./controllers/NoteController');
+const authenticateJWT = require('./middlewares/authenticateJWT');
+const validateUrlParams = require('./middlewares/validateUrlParams');
 
 const routes = Router();
 
 routes.post('/register', UserController.store);
 
 routes.post('/login', SessionController.login);
+
+routes.get('/:username/notes', authenticateJWT, validateUrlParams, NoteController.index);
+routes.get('/:username/notes/:noteId', authenticateJWT, validateUrlParams, NoteController.show);
+routes.post('/:username/notes/', authenticateJWT, validateUrlParams, NoteController.store);
+routes.put('/:username/notes/:noteId', authenticateJWT, validateUrlParams, NoteController.update);
+routes.delete('/:username/notes/:noteId', authenticateJWT, validateUrlParams, NoteController.delete);
 
 module.exports = routes;
