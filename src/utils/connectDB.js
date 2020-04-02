d0ffeff83904bbe84a60caafa5d33300422e5947
Paulo Renato <paulorsouzaf@gmail.com>
2020-04-02 20:12:40 -0300

    Add create index option to mongo connection

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -4,6 +4,7 @@ function connectDB() {
   mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useFindAndModify: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
   });
 }
